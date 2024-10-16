385fcf89dd60215416e561f1e2224516fa482ea4
Nguyen Tuan Long <60863790+ntlong81@users.noreply.github.com>
2024-10-16 14:48:02 +0700

    1610

--- our_module.py
+++ our_module.py
@@ -18,3 +18,47 @@ def gcd(a,b):
         b = r
     return a 
 print('Our_module is imported')
+
+## classes
+class Stack:
+    def __init__(self):
+        self.__stack = []
+    def push(self, item):
+        return self.__stack.append(item)
+    def is_empty(self):
+        return not bool(self.__stack)
+    def size(self):
+        return len(self.__stack)
+    def pop(self):
+        if self.is_empty():
+            return "the stack is empty"
+        else:
+            return self.__stack.pop()
+    def peek(self):
+        if self.is_empty():
+            return "the stack is empty"
+        else:
+            return self.__stack[-1]
+    def __str__(self):
+        return str(self.__stack)
+
+
+class Queue:
+    def __init__(self) -> None:
+        self.__queue = []
+    def enqueue(self,item):
+        return self.__queue.insert(0,item)
+    def dequeue(self):
+        if self.is_empty():
+            return 'The queue is empty'
+        else:
+            return self.__queue.pop()
+    def front(self):
+        if self.is_empty():
+            return 'The queue is empty'
+        else:
+            return self.__queue[-1]
+    def is_empty(self):
+        return not bool(self.__queue)
+    def __str__(self):
+        return str(self.__queue)
